d1db6dc6efe1eec339c474a30957c4ca06708ef2
Vazcuus <levkonst12@gmail.com>
2024-10-16 23:07:12 +0700

    Some methods for future weapon class

--- source/headers/Player.h
+++ source/headers/Player.h
@@ -4,6 +4,9 @@
 #include <memory>
 class PlayerController;
 
+class Weapon;
+enum class WeaponType;
+
 enum class State {
     Idle, 
     Run
@@ -14,10 +17,13 @@ class Player : public Character
     private:
         State d_state;
         std::unique_ptr<PlayerController> d_controller;
+        std::shared_ptr<Weapon> d_weapon;
     public:
         Player() = delete;
         Player(const sf::Texture& texture, sf::Vector2f start_pos, float health);
         ~Player() = default;
         void Update(float time) override;
+        void initWeapon(WeaponType weapon_type, float damage, const sf::Texture& texture);
+        void AttackEnemies(float time);
         void setState(State state);
 };
