f3dfc7871e0760f7f5507f6d46ffa7cbaffe9590
Vazcuus <levkonst12@gmail.com>
2024-10-16 22:36:44 +0700

    Header GameObject

---
+++ source/headers/GameObject.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class GameObject 
+{
+    protected:
+        sf::Vector2f d_size;
+        sf::Vector2f d_pos;
+        sf::Sprite d_sprite;
+
+        GameObject() = default;
+        virtual ~GameObject() = default;
+
+    public:
+        sf::Sprite getSprite() const;
+        sf::Vector2f getPos() const;
+        sf::Vector2f getSize() const;
+}
