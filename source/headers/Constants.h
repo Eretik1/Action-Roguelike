6f0ef1c29d6ae49aeab713122b28b547285e90ba
Vazcuus <levkonst12@gmail.com>
2024-10-16 23:06:09 +0700

    Some new constants for bullet, player, ...

--- source/headers/Constants.h
+++ source/headers/Constants.h
@@ -1,5 +1,17 @@
 #pragma once
 #include <iostream>
+constexpr float PI = 3.14159265;
 
 constexpr float WINDOW_HEIGHT = 1080;
 constexpr float WINDOW_WIDTH = 1920;
+
+constexpr float SPRITE_SIZE = 64.0;
+
+constexpr float PLAYER_SPEED              = 0.12;
+
+constexpr float PLAYER_START_X            = WINDOW_HEIGHT / 2.0 - SPRITE_SIZE;
+constexpr float PLAYER_START_Y            = WINDOW_WIDTH / 2.0 - SPRITE_SIZE;
+
+constexpr float FIREBALL_DEFAULT_SIZE = 24.0;
+constexpr float FIREBALL_DEFAULT_DAMAGE = 20.0;
+constexpr float FIREBALL_DEFAULT_SPEED = 0.24;
