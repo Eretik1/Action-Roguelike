79f5649f301932ee230b71053de2b426d144d0d3
Vazcuus <levkonst12@gmail.com>
2024-10-16 22:36:30 +0700

    Header FireBall

---
+++ source/headers/FireBall.h
@@ -0,0 +1,14 @@
+#include "GameObject.h"
+
+class FireBall : public GameObject 
+{
+    private:
+        sf::Vector2f d_direction;
+        float d_damage;
+    public:
+        FireBall(const sf::Texture& texture, sf::Vector2f pos, sf::Vector2f direction);
+        void Update(float time);
+        bool checkCollisionWithCharacters();
+        bool checkCollisionWithMapBorders();
+        void updateDamage(float scale);
+};
